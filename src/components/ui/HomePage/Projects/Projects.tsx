84b4d690055f1e22044fb44be1b3901988a00333
jannatunNimeNishat <nishatnime100@gmail.com>
2024-07-03 01:28:49 +0600

    Updated

--- src/components/ui/HomePage/Projects/Projects.tsx
+++ src/components/ui/HomePage/Projects/Projects.tsx
@@ -10,7 +10,7 @@ const Projects = async() => {
     });
     const projectSectionData = await res.json();
   return (
-    <div className="  my-24 relative    flex items-center justify-center text-center lg:text-start lg:justify-start text-white">
+    <div className="  py-24 relative    flex items-center justify-center text-center lg:text-start lg:justify-start text-white">
         <div className="lg:w-8/12 mx-auto px-5 lg:px-0 ">
         <div className="text-center bg-gray-700 text-white px-3 py-1 rounded transform my-8 w-3/4 mx-auto block lg:hidden ">
           MY PROJECTS
