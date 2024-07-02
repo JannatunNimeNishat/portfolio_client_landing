b868d0335ca7437a5b7adc103ee9d437c3b5718f
jannatunNimeNishat <nishatnime100@gmail.com>
2024-07-02 20:49:42 +0600

    Updated

--- src/components/ui/HomePage/AboutSection/AboutSection.tsx
+++ src/components/ui/HomePage/AboutSection/AboutSection.tsx
@@ -18,9 +18,9 @@ const AboutSection = async () => {
 
   return (
     <div className=" min-h-[85vh]  relative    flex items-center justify-center text-center lg:text-start lg:justify-start text-white">
-      <div className="w-8/12 mx-auto grid grid-cols-5 gap-12">
-        <div className="col-span-3 flex flex-col justify-center">
-          <p className="text-[18px] leading-[32px] text-justify">{desc}</p>
+      <div className=" w-full px-5 lg:px-0  lg:w-8/12 mx-auto grid  lg:grid-cols-5 gap-12">
+        <div className=" lg:col-span-3 flex flex-col justify-center order-2 lg:order-1 w-full">
+          <p className="text-[18px] leading-[32px]  ">{desc}</p>
           <div className="mt-6">
             <a
               href="https://drive.google.com/uc?export=download&id=1TvIc0eCG1-Hu_5kkaaNs1JlEmTNOAkL-"
@@ -30,11 +30,11 @@ const AboutSection = async () => {
             </a>
           </div>
         </div>
-        <div className="col-span-2">
+        <div className=" lg:col-span-2 order-1 lg:order-2 w-full ">
           <Image
-            className="rounded-md grayscale hover:grayscale-0 transition-all hover:scale-105 duration-200 ease-in-out"
-            width={400}
-            height={400}
+            className=" w-full h-full lg:w-[430px] lg:h-[430px] mx-auto rounded-md grayscale hover:grayscale-0 transition-all lg:hover:scale-105 duration-200 ease-in-out"
+            width={430}
+            height={430}
             src={photoUrl}
             alt="my image"
           />
