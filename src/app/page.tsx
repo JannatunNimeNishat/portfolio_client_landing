4af9bff5e70ba738e6b45b86c68b6433fd463baa
jannatunNimeNishat <nishatnime100@gmail.com>
2024-07-02 20:37:09 +0600

    About section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
+import AboutSection from "@/components/ui/HomePage/AboutSection/AboutSection";
 import HeroSection from "@/components/ui/HomePage/HeroSection/HeroSection";
 import SkillSection from "@/components/ui/HomePage/SkillSection/SkillSection";
 
-
 export default function Home() {
   return (
-    <div 
+    <div
     // className="max-w-[1440px]  mx-auto text-white"
-  
     >
-      <HeroSection/>
-      <SkillSection/>
+      <HeroSection />
+      <AboutSection />
+      <SkillSection />
     </div>
   );
 }
