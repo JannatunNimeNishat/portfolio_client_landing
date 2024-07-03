3396949b00ed491b24f5b3b555f060b992eec9da
jannatunNimeNishat <nishatnime100@gmail.com>
2024-07-03 17:57:10 +0600

    Footer added

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,9 +1,19 @@
+import Link from "next/link";
 
 
 const Footer = () => {
     return (
-        <div>
-            footer
+        <div className=" bg-[#111a28] text-white py-8">
+            <div className="flex items-center justify-between w-8/12 mx-auto">
+
+            <div className="flex items-center gap-4">
+                <Link className="hover:text-green-500 duration-200 ease-out" href={"https://github.com/JannatunNimeNishat"} target="_blank">GITHUB</Link>
+                <Link className="hover:text-green-500 duration-200 ease-out" href={"https://www.linkedin.com/in/jannatun-nime/"} target="_blank">LINKEDIN</Link>
+                <Link className="hover:text-green-500 duration-200 ease-out" href={"https://x.com/NimeNishat"} target="_blank">TWITTER</Link>
+                <Link className="hover:text-green-500 duration-200 ease-out" href={"https://www.facebook.com/jannatunbd/"} target="_blank">FACEBOOK</Link>
+            </div>
+            <p className="hover:text-green-500 duration-200 ease-out">Md.Jannatun Nime </p>
+            </div>
         </div>
     );
 };
